117094913208129084b27e03ebfc934078186249
Matt Hurst <matt@qrl.app>
2018-10-17 08:56:19 -0700

    Update example with more variety

--- example/main.dart
+++ example/main.dart
@@ -3,17 +3,11 @@ import 'package:just_debounce_it/just_debounce_it.dart';
 void main() {
   void printSeconds() => print("Hello World (Debounce.seconds)");
   Debounce.seconds(1, printSeconds);
-  Debounce.seconds(1, printSeconds);
-  Debounce.seconds(1, printSeconds);
-  Debounce.seconds(1, printSeconds);
-  Debounce.seconds(1, printSeconds);
+  Debounce.runAndClear(printSeconds); // Will print immediately
 
   void printMs() => print("Hello World (Debounce.milliseconds)");
   Debounce.milliseconds(500, printMs);
-  Debounce.milliseconds(500, printMs);
-  Debounce.milliseconds(500, printMs);
-  Debounce.milliseconds(500, printMs);
-  Debounce.milliseconds(500, printMs);
+  Debounce.clear(printMs); // Will not run
 
   final duration = const Duration(milliseconds: 2000);
   Debounce.duration(duration, print, ["Hello World (Debounce.duration)"]);
